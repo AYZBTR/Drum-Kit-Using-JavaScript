294ad0b6577f4b93d9fd45494e7b1471d8286800
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2024-01-21 01:54:06 +0200

    applying Switch statement

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
-/* 
-
-document.querySelector("button").addEventListener("click",handleClick)
-function handleClick(){
-    alert("clicekd")
-} */
-
 var buttons = document.querySelectorAll(".drum");
 
 buttons.forEach(function(buttons){
     buttons.addEventListener("click", function(){
-        var audio = new Audio("sounds/crash.mp3")
-        audio.play();
-    })
-})
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "a":
+                var audioTom1 = new Audio("sounds/tom-1.mp3")
+                audioTom1.play(); 
+                break;
+            case "s":
+                var audioTom2 = new Audio("sounds/tom.2.mp3")
+                audioTom2.play(); 
+                break;
+
+            case "d":
+                var audioTom2 = new Audio("sounds/tom.2.mp3")
+                audioTom2.play(); 
+                break;
+
+            case "f":
+                var audioTom2 = new Audio("sounds/tom.2.mp3")
+                audioTom2.play(); 
+                break;
+
+            case "j":
+                var audioTom2 = new Audio("sounds/tom.2.mp3")
+                audioTom2.play(); 
+                break;
+
+            case "k":
+                var audioTom2 = new Audio("sounds/tom.2.mp3")
+                audioTom2.play(); 
+                break;
+
+            case "ljak":
+                var audioTom2 = new Audio("sounds/tom.2.mp3")
+                audioTom2.play(); 
+                break;
+
+
+
+            default:
+                break;
+        }
+         
+    });
+}) 
+ 
+
+
+
+
+ var audio = new Audio("sounds/crash.mp3")
+        audio.play(); 
