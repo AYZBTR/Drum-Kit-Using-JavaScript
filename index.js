5dba806033d8aa4f6360194258807b374ede409c
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2024-01-21 02:37:50 +0200

    perfect

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
+
+//Detecting Button Press
 var buttons = document.querySelectorAll(".drum");
 
 buttons.forEach(function(buttons){
     buttons.addEventListener("click", function(){
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case "a":
-                var audioTom1 = new Audio("sounds/tom-1.mp3")
-                audioTom1.play(); 
-                break;
-            case "s":
-                var audioTom2 = new Audio("sounds/tom-2.mp3")
-                audioTom2.play(); 
-                break;
-
-            case "d":
-                var audioTom2 = new Audio("sounds/tom-3.mp3")
-                audioTom2.play(); 
-                break;
-
-            case "f":
-                var audioTom2 = new Audio("sounds/tom-4.mp3")
-                audioTom2.play(); 
-                break;
-
-            case "j":
-                var audioTom2 = new Audio("sounds/snare.mp3")
-                audioTom2.play(); 
-                break;
-
-            case "k":
-                var audioTom2 = new Audio("sounds/crash.mp3")
-                audioTom2.play(); 
-                break;
-
-            case "l":
-                var audioTom2 = new Audio("sounds/kick-bass.mp3")
-                audioTom2.play(); 
-                break;
-
-
-
-            default: console.log(buttonInnerHTML)
-                
-        }
+        playDrums(buttonInnerHTML);
          
     });
 }) 
  
+// Detecting Keyboard Press
+document.addEventListener("keypress", function(event){
+    playDrums(event.key);
+
+});
 
 
 
+function playDrums(key){
+    switch (key) {
+        case "a":
+            var audioTom1 = new Audio("sounds/tom-1.mp3")
+            audioTom1.play(); 
+            break;
+        case "s":
+            var audioTom2 = new Audio("sounds/tom-2.mp3")
+            audioTom2.play(); 
+            break;
 
- var audio = new Audio("sounds/crash.mp3")
-        audio.play(); 
+        case "d":
+            var audioTom2 = new Audio("sounds/tom-3.mp3")
+            audioTom2.play(); 
+            break;
+
+        case "f":
+            var audioTom2 = new Audio("sounds/tom-4.mp3")
+            audioTom2.play(); 
+            break;
+
+        case "j":
+            var audioTom2 = new Audio("sounds/snare.mp3")
+            audioTom2.play(); 
+            break;
+
+        case "k":
+            var audioTom2 = new Audio("sounds/crash.mp3")
+            audioTom2.play(); 
+            break;
+
+        case "l":
+            var audioTom2 = new Audio("sounds/kick-bass.mp3")
+            audioTom2.play(); 
+            break;
+
+
+
+        default: console.log(buttonInnerHTML)
+            
+    }
+}
